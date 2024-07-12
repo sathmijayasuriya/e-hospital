8f9932c599fe66387575d771dd9665690ebc9387
sathmijayasuriya <sathmijayasuriya@gmail.com>
2024-07-12 11:46:17 +0530

    changes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,10 @@ import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import Requests from "./pages/Requests";
+import Settings from "./pages/Settings";
+import Feedbacks from "./pages/Feedbacks";
+import Reports from "./pages/Reports";
+import Patient from "./pages/Patient";
 
 export const router = createBrowserRouter([
     {
@@ -16,5 +20,21 @@ export const router = createBrowserRouter([
       {
         path: "/requests",
         element: <Requests />,
+      },
+      {
+        path: "/settings",
+        element: <Settings />,
+      },
+      {
+        path: "/feedbacks",
+        element: <Feedbacks />,
+      },
+      {
+        path: "/reports",
+        element: <Reports />,
+      },
+      {
+        path: "/patient",
+        element: <Patient />,
       },
 ])
