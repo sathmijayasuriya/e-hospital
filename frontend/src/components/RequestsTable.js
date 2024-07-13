148feee263857cea8a436fc0c443e4eb8b631a10
sathmijayasuriya <sathmijayasuriya@gmail.com>
2024-07-14 00:05:19 +0530

    changes

---
+++ frontend/src/components/RequestsTable.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+
+const columns = [
+    {id :'slNo',label:'SL no'},
+    {id : 'requestId',label:'Request Id'},
+    {id : 'createdOn',label:'Created On'},
+    {id : 'location',label:'Location'},
+    {id : 'service',label:'Service'},
+    {id : 'department',label:'Department'},
+    {id : 'requestBy',label:'Request By'},
+    {id : 'assignTo',label:'Assign To'},
+    {id : 'priority',label:'priority'},
+];
+
+export default function RequestsTable() {
+  return (
+    <>
+    <Box sx={{height:"100%",
+              backgroundColor:"#f2f2f2",
+              width :"100%"
+              }}>
+                hiiiiiiiiiiiiii
+    </Box>
+    </>
+  )
+}
