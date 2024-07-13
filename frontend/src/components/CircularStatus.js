54413acbd344dea38538f55a13ff82c2ee7613af
sathmijayasuriya <sathmijayasuriya@gmail.com>
2024-07-13 19:44:54 +0530

    changes

--- frontend/src/components/CircularStatus.js
+++ frontend/src/components/CircularStatus.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import { styled } from '@emotion/styled'
+import { Paper } from '@mui/material'
+const requestData = [
+  { number: '10', status: 'new Requests'},
+  { number: '05', status: 'Delayed Requests' },
+  { number: '02', status: 'Escalated Requests' },
+  { number: '00', status: 'On Hold Requests' },
+]
+const RequestItem = ({number,status}) =>{
+  return(
+    <Paper
+      sx = {{
+        display:'flex',
+        flexDirection :'column',
+      }}
+    >
+
+    </Paper>
+  )
+
+}
 
 export default function CircularStatus() {
+
+
   return (
     <div>
 
