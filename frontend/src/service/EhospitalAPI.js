c744f543ac8e43952ac3b151aa80327649d9a57d
sathmijayasuriya <sathmijayasuriya@gmail.com>
2024-08-21 16:10:29 +0530

    changes

--- frontend/src/service/EhospitalAPI.js
+++ frontend/src/service/EhospitalAPI.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+import Configuration from "../Configuration";
+
+//fetch request list
+
+export const FetchRequests = async() =>{
+    try {
+        const response = await axios.get(`${Configuration.BASE_URL}/getRequestData`);
+            return response.data;
+
+    }catch (error) {
+        throw error;
+    }
+}
